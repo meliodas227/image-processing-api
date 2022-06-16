1b328e525d77d037698b3ac3e263ee683f7bfc38
meliodas227 <yousefalaa2279@gmail.com>
2022-06-16 19:23:23 +0200

    finished project

---
+++ src/tests/indexSpec.ts
@@ -0,0 +1,23 @@
+import supertest from 'supertest';
+import app from '../index';
+import fs from 'fs'
+import path from 'path';
+
+const request = supertest(app);
+
+describe('is image exist',() => {
+    it('is image existance checker defined', ()=>{
+        expect(fs.existsSync(path.join(__dirname,"..","..","thump-images","icelandwaterfall_300_300.jpg"))).toBe(false)
+    })
+    it('is image saved in thump-images',async () => {
+        await request.get('/resize?name=icelandwaterfall&height=550&width=550')
+        expect(fs.existsSync(path.join(__dirname,"..","..","thump-images","icelandwaterfall_550_550.jpg"))).toBe(true)
+    })
+})
+
+describe('Testing responses', () => {
+    it('getting api endpoint', async () => {
+        const response = await request.get('/resize?name=icelandwaterfall&height=550&width=550');
+        expect(response.status).toBe(200);
+    }
+)});
